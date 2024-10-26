2e9cb4d69ff0b26e31103c559dcf5104e83542b3
Teemu H <temehama@gmail.com>
2024-10-26 13:07:10 +0300

    added kernel.c

---
+++ kernel.c
@@ -0,0 +1,24 @@
+void kmain(void){
+    const char *str = "Kernel goes wrooom";
+    char *vidptr =(char*)0xb8000; //Video memory begins here
+    unsigned int i = 0;
+    unsigned int j = 0;
+
+    while (j < 80 * 25 * 2)
+    {
+        vidptr[j] = ' ';
+        vidptr[j+1] = 0x07;
+        j = j + 2;
+    }
+
+    j = 0;
+    
+    while (str[j] != '\0')
+    {
+        vidptr[i] = str[j];
+        vidptr[i+1] = 0x07;
+        ++j;
+        i = i + 2;
+    }
+    return;
+}
